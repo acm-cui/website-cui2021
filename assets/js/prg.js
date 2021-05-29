6a4cb4dbe1a15483a61533db4d31e5b2922056b9
Martin Porcheron <martin+git@porcheron.uk>
2021-05-29 15:11:49 +0100

    Show end of date messages for people where events are at the start and end of the day

--- assets/js/prg.js
+++ assets/js/prg.js
@@ -24,6 +24,7 @@ var nth = function(d) {
 $(function() {
     $.getJSON('../programme.json?s=' + (new Date().getTime()), function(data) {
         var $prg = $('#programme');
+        var num_sessions = 0;
         var sessions_by_date = {};
 
         $.each(data, function(i, session) {
@@ -34,6 +35,7 @@ $(function() {
                 sessions_by_date[start_date_index] = [];
             }
             sessions_by_date[start_date_index].push(session);
+            num_sessions++;
         });
         $('#numdays').html(Object.keys(sessions_by_date).length);
         $('.timezone').text('converted to your operating system\'s timezone (' + Intl.DateTimeFormat().resolvedOptions().timeZone + ')');
@@ -49,6 +51,7 @@ $(function() {
             index++;
         });
 
+        var global_session_id = 0;
         index = 0;
         $.each(sessions_by_date, function(date_string, sessions) {
             html = '<div class="card"><div class="card-header bg-dark" id="programmeDay' + index +'">';
@@ -58,64 +61,86 @@ $(function() {
             html += '<ul class="list-unstyled prg-day mb-0 border-0 rounded-0">';
 
             $.each(sessions, function(session_id, session) {
-            if (session['type'] == 'Break') {
-                html += '<li class="media p-3 bg-light border-bottom rounded-0" id="session-' + index + '"><div class="media-body text-center text-muted">';
-                if (session['description'] != '') {
-                    html += session['description'];
-                }
-                html += '</div></li>';
-            } else {
-                html += '<li class="media prg-row p-3 rounded bg-light border-bottom rounded-0 flex-md-row flex-column" id="session-' + session['id'] + '"><div class="mr-md-3 mb-md-0 mb-3"><div class="badge badge-primary text-capitalize mb-2 mr-3">' + session['type'] + '</div>';
-                html += '<div class="mb-1 small"><span alt="A clock" class="d-inline-block prg-icon-timing prg-icon-start mr-2"></span><span class="d-inline-block prg-text-timing">Starts at <span class="prg-timing"><span></span>' + timestr(new Date(Date.parse(session['start_time']))) +'</span></div>';
-                html += '<div class="mt-1 small"><span alt="A stopwatch" class="d-inline-block prg-icon-timing prg-icon-end mr-2"></span><span class="d-inline-block prg-text-timing">Ends at <span class="prg-timing"><span></span>' + timestr(new Date(Date.parse(session['end_time']))) +'</span>';
-                html += '</div></div>';
-                html += '<div class="media-body w-100">';
-                if (session['youtube'] != '') {
-                    html += '<div class="float-right"><a href="' + session['youtube'] + '" title="Watch the session on YouTube" class="d-block prg-icon-yt mr-4"><span class="sr-only">Watch on YouTube</span></a></div>';
-                }
-                html += '<h4 class="text-primary mt-0 mb-1">' + session['title'] + '</h4>';
+                global_session_id++;
+                if (session['type'] == 'Break') {
+                    if (session_id > 0 && session_id < sessions.length-1) {
+                        html += '<li class="media p-3 bg-light border-bottom rounded-0" id="session-' + index + '"><div class="media-body text-center text-muted">';
+                        if (session['description'] != '') {
+                            html += session['description'];
+                        }
+                        html += '</div></li>';
+                    } else {
+                        html += '<li class="media p-3 bg-light border-bottom rounded-0" id="session-' + index + '"><div class="media-body text-center text-muted">';
+                        html += 'Please see the next day\'s schedule as events may start soon after midnight';
+                        html += '</div></li>';
+                    }
+                } else if (session['type'] == 'EOD') {
+                    if (session_id > 0 && session_id < sessions.length-1) {
+                        html += '<li class="media p-3 bg-light border-bottom rounded-0" id="session-' + index + '"><div class="media-body text-center text-muted">';
+                        if (session['description'] != '') {
+                            html += session['description'];
+                        }
+                        html += '</div></li>';
+                    }
+                } else {
+                    html += '<li class="media prg-row p-3 rounded bg-light border-bottom rounded-0 flex-md-row flex-column" id="session-' + session['id'] + '"><div class="mr-md-3 mb-md-0 mb-3"><div class="badge badge-primary text-capitalize mb-2 mr-3">' + session['type'] + '</div>';
+                    html += '<div class="mb-1 small"><span alt="A clock" class="d-inline-block prg-icon-timing prg-icon-start mr-2"></span><span class="d-inline-block prg-text-timing">Starts at <span class="prg-timing"><span></span>' + timestr(new Date(Date.parse(session['start_time']))) +'</span></div>';
+                    html += '<div class="mt-1 small"><span alt="A stopwatch" class="d-inline-block prg-icon-timing prg-icon-end mr-2"></span><span class="d-inline-block prg-text-timing">Ends at <span class="prg-timing"><span></span>' + timestr(new Date(Date.parse(session['end_time']))) +'</span>';
+                    html += '</div></div>';
+                    html += '<div class="media-body w-100">';
+                    if (session['youtube'] != '') {
+                        html += '<div class="float-right"><a href="' + session['youtube'] + '" title="Watch the session on YouTube" class="d-block prg-icon-yt mr-4"><span class="sr-only">Watch on YouTube</span></a></div>';
+                    }
+                    html += '<h4 class="text-primary mt-0 mb-1">' + session['title'] + '</h4>';
 
-                if (session['presenters'] != '') {
-                    html += session['presenters'];
-                }
+                    if (session['presenters'] != '') {
+                        html += session['presenters'];
+                    }
 
-                if (session['chairs'] != '') {
-                    html += '<em class="small">Chaired by ' + session['chairs'] + '</em>';
-                }
+                    if (session['chairs'] != '') {
+                        html += '<em class="small">Chaired by ' + session['chairs'] + '</em>';
+                    }
 
-                if (session['presentations'].length > 0) {
-                    html += '<ol class="list-group mt-3">';
-                    $.each(session['presentations'], function(presentation_id) {
-                        presentation = session['presentations'][presentation_id]
-                        html += '<li class="list-group-item pb-3">';
-                        if (presentation['aaai']) {
-                            html += '<div class="float-right">';
-                            html += '<a href="' + presentation['aaai'] + '" title="View the paper on the AAAI website" class="d-block prg-inner-icon prg-icon-aaai"><span class="sr-only">View paper on the AAAI website</span></a>';
-                            html += '</div>';
-                        }
-                        if (presentation['acmdl']) {
-                            html += '<div class="float-right">';
-                            html += '<a href="' + presentation['acmdl'] + '" title="View the paper in the ACM Digital Library" class="d-block prg-inner-icon prg-icon-acmdl"><span class="sr-only">View paper in the ACM Digital Library</span></a>';
-                            html += '</div>';
-                        }
-                        if (presentation['sagepub']) {
-                            html += '<div class="float-right">';
-                            html += '<a href="' + presentation['sagepub'] + '" title="View the paper on Sage Publishing\'s website" class="d-block prg-inner-icon prg-icon-sagepub"><span class="sr-only">View paper on the Sage Publishing\'s website</span></a>';
-                            html += '</div>';
-                        }
-                        if (presentation['springerlink']) {
-                            html += '<div class="float-right">';
-                            html += '<a href="' + presentation['springerlink'] + '" title="View the paper on Springer\'s website" class="d-block prg-inner-icon prg-icon-springerlink"><span class="sr-only">View paper on Springer\'s website</span></a>';
-                            html += '</div>';
-                        }
-                        html += '<strong>' + presentation['title'] + (presentation['type'] != 'Panel' ? ' <span class="badge badge-secondary">' + presentation['type'] + '</span>' : '') + '</strong><br>';
-                        html += presentation['authors'] + '</li>';
-                    });
-                    html += '</ol>';;
-                }
+                    if (session['presentations'].length > 0) {
+                        html += '<ol class="list-group mt-3">';
+                        $.each(session['presentations'], function(presentation_id) {
+                            presentation = session['presentations'][presentation_id]
+                            html += '<li class="list-group-item pb-3">';
+                            if (presentation['aaai']) {
+                                html += '<div class="float-right">';
+                                html += '<a href="' + presentation['aaai'] + '" title="View the paper on the AAAI website" class="d-block prg-inner-icon prg-icon-aaai"><span class="sr-only">View paper on the AAAI website</span></a>';
+                                html += '</div>';
+                            }
+                            if (presentation['acmdl']) {
+                                html += '<div class="float-right">';
+                                html += '<a href="' + presentation['acmdl'] + '" title="View the paper in the ACM Digital Library" class="d-block prg-inner-icon prg-icon-acmdl"><span class="sr-only">View paper in the ACM Digital Library</span></a>';
+                                html += '</div>';
+                            }
+                            if (presentation['sagepub']) {
+                                html += '<div class="float-right">';
+                                html += '<a href="' + presentation['sagepub'] + '" title="View the paper on Sage Publishing\'s website" class="d-block prg-inner-icon prg-icon-sagepub"><span class="sr-only">View paper on the Sage Publishing\'s website</span></a>';
+                                html += '</div>';
+                            }
+                            if (presentation['springerlink']) {
+                                html += '<div class="float-right">';
+                                html += '<a href="' + presentation['springerlink'] + '" title="View the paper on Springer\'s website" class="d-block prg-inner-icon prg-icon-springerlink"><span class="sr-only">View paper on Springer\'s website</span></a>';
+                                html += '</div>';
+                            }
+                            html += '<strong>' + presentation['title'] + (presentation['type'] != 'Panel' ? ' <span class="badge badge-secondary">' + presentation['type'] + '</span>' : '') + '</strong><br>';
+                            html += presentation['authors'] + '</li>';
+                        });
+                        html += '</ol>';;
+                    }
                 
-                html +'</div></li>';
-            }
+                    html +'</div></li>';
+
+                    if (session_id == sessions.length-1 && global_session_id < num_sessions) {
+                        html += '<li class="media p-3 bg-light border-bottom rounded-0" id="session-' + index + '"><div class="media-body text-center text-muted">';
+                        html += 'Please see the next day\'s schedule as events may start soon after midnight';
+                        html += '</div></li>';
+                    }
+                }
+
             });
 
             html += '</ul></div>';
